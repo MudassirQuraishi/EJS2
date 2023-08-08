123c1e9bb25fa677737471fd78628eb063d75dd5
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-08-08 08:58:58 +0530

    task10

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,10 @@
+const path = require('path');
 
 const express = require('express');
-
 
 const shopController = require('../controllers/shop');
 
 const router = express.Router();
-
 
 router.get('/', shopController.getIndex);
 
@@ -14,6 +13,10 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 router.get('/orders', shopController.getOrders);
 
