08c2145a527a427c7ba92483469483deb2f71763
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-08-08 09:49:20 +0530

    database connected to node js server

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'H3lloworld!'
+});
+
+module.exports = pool;
